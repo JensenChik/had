f54dba086747c2835a477de24f498f96d2bb3a0f
Jensen <suiterchik@gmail.com>
2019-08-11 00:23:02 +0800

    change class name SQLType to DataType

--- src/test/kotlin/tech/cuda/shared/DataTypeTest.kt
+++ src/test/kotlin/tech/cuda/shared/DataTypeTest.kt
@@ -8,6 +8,14 @@ class SQLTypeTest : StringSpec({
     "bigint should be number" {
         SQLType.BIGINT.isInteger() shouldBe true
     }
+
+    "bigint should be cast to timestamp"{
+        SQLType.BIGINT canBeCastTo SQLType.TIMESTAMP shouldBe true
+    }
+
+    "bigint to string should be BIGINT"{
+        "${SQLType.BIGINT}" shouldBe "BIGINT"
+    }
 })
 
 
