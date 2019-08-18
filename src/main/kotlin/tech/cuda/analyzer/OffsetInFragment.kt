ef363ba343ac4b09cb186c3e698166c2a1992652
Jensen <suiterchik@gmail.com>
2019-08-19 00:46:39 +0800

    init offset in fragment

---
+++ src/main/kotlin/tech/cuda/analyzer/OffsetInFragment.kt
@@ -0,0 +1,42 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package tech.cuda.analyzer
+
+import tech.cuda.enums.ColumnType
+
+/**
+ * The offset of a row in the current fragment. To be used by updates.
+ */
+class OffsetInFragment : Expression(ColumnType.INT) {
+    override fun deepCopy(): Expression {
+        return OffsetInFragment()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return javaClass.hashCode()
+    }
+
+    override fun toString(): String {
+        return "{OffsetInFragment)"
+    }
+
+
+}
+
